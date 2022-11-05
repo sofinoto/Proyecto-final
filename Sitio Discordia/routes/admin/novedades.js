1c115cf789deef824e7464ac0f0bf22d1255088b
Sofia Noto <sofianoto1507@gmail.com>
2022-11-05 14:37:23 -0300

    ok

--- Sitio Discordia/routes/admin/novedades.js
+++ Sitio Discordia/routes/admin/novedades.js
@@ -48,4 +48,37 @@ router.post('/agregar', async (req, res, next) => {
 });
 
 
+router.get('/modificar/:id', async (req, res, next) => {
+    var id = req.params.id;
+    console.log(req.params.id);
+    var novedad = await novedadesmodel.getNovedadById(id);
+
+    res.render('admin/modificar', {
+        layout: 'admin/layout',
+        novedad
+    })
+});
+
+
+router.post('/modificar', async (req, res, next) => {
+    try {
+        console.log(obj)
+        var obj = {
+            titulo: req.body.titulo,
+            subtitulo: req.body.subtitulo,
+            cuerpo: req.body.cuerpo,
+        }
+await novedadesmodel.modificarNovedadById(obj, req.body.id);
+res.redirect('/admin/novedades');
+
+   } catch (error) {
+        console.log(error)
+        res.render('admin/modificar', {
+            layout: 'admin/layout',
+            error: true,
+            message: 'No se modifico la novedad'
+        })
+    }
+})
+
 module.exports = router; 
